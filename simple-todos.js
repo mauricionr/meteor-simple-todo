903bde4c1358ceca2bd7d1cb9031323a8d3a00b8
root <mauricioskateboard@gmail.com>
2015-08-21 17:58:50 -0300

    identando

--- simple-todos.js
+++ simple-todos.js
@@ -2,36 +2,50 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
+  Template.body.helpers(
+    {
+      tasks: 
+        function () {
+          // Show newest tasks at the top
+          return Tasks.find({}, {
+              sort: {
+                  createdAt: -1
+                 }
+            });
+      }
     }
-  });
+  );
   Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-      // Get value from form element
-      var text = event.target.text.value;
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        checked:false// current time
-      });
-      // Clear form
-      event.target.text.value = "";
+    "submit .new-task": 
+      function (event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+        // Get value from form element
+        var text = event.target.text.value;
+        // Insert a task into the collection
+        Tasks.insert(
+          {
+            text: text,
+            createdAt: new Date(),
+            checked:false// current time
+          }
+        );
+        // Clear form
+        event.target.text.value = "";
     }
   });
   Template.task.events({
-    "click .toggle-checked": function (){
-      Tasks.update(this._id, {
-        $set: {checked:this.checked ? false : true}
-      });
+    "click .toggle-checked": 
+      function (){
+        Tasks.update(this._id, {
+          $set: {
+              checked:this.checked ? false : true
+            }
+        });
     },
-    "click .delete": function (){
-      Tasks.remove(this._id);
-    }
+    "click .delete": 
+      function (){
+        Tasks.remove(this._id);
+      }
   });
 }
